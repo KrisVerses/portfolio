0f1b652ef8ccb7f1a4edbdad1b50e1a8c15b6a3e
KrisVerses <krisincode@email.com>
2025-03-18 17:46:55 -0500

    styles: add navbar styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1A3C40",
+        secondary: "#14171A",
+        accent: "#FF6F61",
+        background: "#E8ECEF",
+        border: "#E1E8ED",
+        text: "#0F1419",
+        complementary1: "#61FF6F", // Complementary color to accent
+        complementary2: "#3C401A", // Complementary color to primary
+      },
+      scale: {
+        110: "1.1",
+      },
+    },
   },
   plugins: [],
 };
